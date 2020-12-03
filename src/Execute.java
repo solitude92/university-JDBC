0cde2fc615ce2a413b83d00e18c66dba1f0fde85
solitude92 <m.solitude92@gmail.com>
2020-12-04 02:23:12 +0330

    Add printMenu an executiveMenu method to figure the interface of the application in console and some minor changes to get a better results.

--- src/Execute.java
+++ src/Execute.java
@@ -4,11 +4,11 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Execute {
+    private static String tableName;
+    private static String dbName;
+    private static String password;
     public static void main(String[] args) {
 
-        String dbName;
-        String password;
-        String tableName;
         boolean isTable = true;
 
         while (true) {
@@ -16,16 +16,16 @@ public class Execute {
             password = scanner().next();
             System.out.println("Enter database name: ");
             dbName = scanner().next();
+            System.out.println("Enter table name: ");
+            tableName = scanner().next();
+            SqlFunctions sqlFunctions = new SqlFunctions(dbName, password);
             while (isTable) {
-                System.out.println("Enter table name: ");
-                tableName = scanner().next();
 
                 try {
-                    SqlFunctions sqlFunctions = new SqlFunctions(dbName, password);
                     sqlFunctions.showTable(tableName);
-                    sqlFunctions.editInTable(tableName);
-                    sqlFunctions.addToTable(tableName);
-                    sqlFunctions.deleteFromTable(tableName);
+                    printMenu();
+                    executiveMenu(sqlFunctions, tableName);
+
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -36,5 +36,53 @@ public class Execute {
     public static Scanner scanner() {
         Scanner scanner = new Scanner(System.in);
         return scanner;
+    }
+
+    public static void printMenu(){
+        System.out.println();
+        System.out.println("=*=*=*=*=*==*=*=*=*=*=*=*=*==*=*=*=*=*=*=*=*=*==*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+        System.out.println("What do you want to do?(Enter the number:)");
+        System.out.println("1)Show the table the table");
+        System.out.println("2)Add to the table");
+        System.out.println("3)Delete from the table");
+        System.out.println("4)Edit in the table");
+        System.out.println("5)Select another table");
+        System.out.println("6)Select another data base");
+        System.out.println("=*=*=*=*=*==*=*=*=*=*=*=*=*==*=*=*=*=*=*=*=*=*==*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+        System.out.println();
+    }
+    public static void executiveMenu(SqlFunctions sqlFunctions , String tableName) throws SQLException {
+
+        int action = scanner().nextInt();
+        switch (action){
+            case 1:
+                //To show another table at the same time of current table
+                //It's help full while changing the middle table
+                System.out.println("Enter another table name to show:(NOTE:operations won't" +
+                        " affect this table unless you use '5' to change the table) ");
+                sqlFunctions.showTable(scanner().next());
+                break;
+            case 2:
+                sqlFunctions.addToTable(tableName);
+                break;
+            case 3:
+                sqlFunctions.deleteFromTable(tableName);
+                break;
+            case 4:
+                sqlFunctions.editInTable(tableName);
+                break;
+            case 5:
+                System.out.println("Enter table name: ");
+                Execute.tableName = scanner().next();
+                break;
+            case 6:
+                System.out.println("Enter database name: ");
+                sqlFunctions.setDbName(scanner().next());
+                System.out.println("Enter table name: ");
+                Execute.tableName = scanner().next();
+                break;
+            default:
+                System.out.println("Invalid input!!!!");
+        }
     }
 }
